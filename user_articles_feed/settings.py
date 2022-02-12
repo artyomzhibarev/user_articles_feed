4da760742ca1a5466d13fd075b3199b16160c163
artyomzhibarev <artyom.zhibarev@yandex.ru>
2022-02-12 13:53:43 +0300

    Update static_root

--- user_articles_feed/settings.py
+++ user_articles_feed/settings.py
@@ -143,9 +143,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
