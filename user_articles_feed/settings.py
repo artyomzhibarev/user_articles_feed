6249fa9c62b6277705548dc245be128a932f7de7
artyomzhibarev <artyom.zhibarev@yandex.ru>
2022-02-12 14:40:57 +0300

    Added django-heroku

--- user_articles_feed/settings.py
+++ user_articles_feed/settings.py
@@ -10,17 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-
+import django_heroku
 import django.contrib.auth.password_validation
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-
 
 
 # Quick-start development settings - unsuitable for production
@@ -167,3 +165,4 @@ DEFAULT_RENDERER_CLASSES = [
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES,
 }
+django_heroku.settings(locals())
