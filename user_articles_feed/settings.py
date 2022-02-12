2ea69e6cbb573563ae4c302722243412abad227f
artyomzhibarev <artyom.zhibarev@yandex.ru>
2022-02-12 14:08:16 +0300

    change settings.py

--- user_articles_feed/settings.py
+++ user_articles_feed/settings.py
@@ -18,7 +18,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 
