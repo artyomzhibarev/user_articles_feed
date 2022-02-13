1df7eb20bf57a93c38b0cdb10271b1a509e708f2
artyomzhibarev <artyom.zhibarev@yandex.ru>
2022-02-13 15:32:34 +0300

    dfsf

--- user_articles_feed/settings.py
+++ user_articles_feed/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = 'django-insecure--0i@=&e)q7($40+lxlc&kw((8e$sws3cleovmz^7-4g7@+)x*m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'feed.User'
